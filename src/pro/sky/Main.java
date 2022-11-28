ecb4bb2f67e0c00209e41844364b40781f9538a6
Kommit <aksaghovafatima@gmail.com>
2022-11-28 18:05:28 +0300

    дз6

---
+++ src/pro/sky/Main.java
@@ -0,0 +1,84 @@
+package pro.sky;
+public class Main {
+    public static void main(String[] args) {
+        // Блок 1 : Заданиен 1
+        System.out.println("Задание 1");
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Итерация цикла" + " " + i);
+            System.out.println("===================================");
+
+            // Задание 2
+            System.out.println("Задание 2");
+        }
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("Итерация цикла" + " " + i);
+
+            System.out.println("===================================");
+
+        }
+            // Задание 3
+            System.out.println("Задание 3");
+
+          for (int i = 2; i <= 17; i += 2) {
+              System.out.println(i);
+
+              System.out.println("===========================================");
+
+
+              // Задание 4
+          }
+              System.out.println("Задание 4");
+
+              for (int i = 10; i > -10; i--) {
+                  System.out.println("Итерация цикла" + " " + i);
+
+                  System.out.println("===================================");
+
+              }
+                  //  Блок 1 : Задание 1
+                  System.out.println("Задание 1");
+
+                  for (int i = 1904; i < 2096; i = i + 4) {
+                      System.out.println("Висакосный год " + " " + i);
+                      System.out.println("===============================================");
+
+                  }
+                      // Задание 2
+                      System.out.println("Задание 2");
+
+                  for (int i = 7; i <= 98; i = i + 7) {
+                      System.out.println(i);
+
+                      System.out.println("=======================================");
+
+                      // Задание 3
+                  }
+                      for (int i = 1; i <= 512; i = i + i++) {
+                          System.out.println(i);
+
+                          // Блок 3 Задание 1
+                          System.out.println("Задание 4");
+
+                      }
+                          int salary = 29000;
+                          int total = 0;
+                          for (int i = 0; i < 12; i++){
+                              total = total + total/100;
+                              total = total + salary;
+                              System.out.println("Месяц" + " " + salary + " " +  "сумма накоплений равна"+ " " +  total + " " +"рублей");
+
+
+
+                  }
+
+
+    }
+
+
+
+
+
+    }
